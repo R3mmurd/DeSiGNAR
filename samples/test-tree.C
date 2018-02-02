13912ce46c033ac0645c038d19a4955b43c4470b
Alejandro <Alejandro>
2018-02-02 19:00:57 -0400

    test cartesian product with an array set

--- samples/test-tree.C
+++ samples/test-tree.C
@@ -143,20 +143,23 @@ int main()
   assert(tree.all([] (auto item) { return item % 2 == 0; }));
   assert(not tree.exists([] (auto item) { return item % 2 != 0; }));
 
-  TreeSet<lint_t> s1 = {1,2,3,4};
-  TreeSet<lint_t> s2 = {3,4,5,6};
+  TreeSet<lint_t>  ts1 = {1,2,3,4};
+  TreeSet<lint_t>  ts2 = {3,4,5,6};
+  ArraySet<lint_t> as2 = {3,4,5,6};
 
-  assert(s1.join(s2).equal({1,2,3,4,5,6}));
+  assert(ts1.join(ts2).equal({1,2,3,4,5,6}));
 
-  assert(s1.intersect(s2).equal({3,4}));
+  assert(ts1.intersect(ts2).equal({3,4}));
 
-  assert(s1.difference(s2).equal({1,2}));
+  assert(ts1.difference(ts2).equal({1,2}));
 
-  assert(s2.difference(s1).equal({5,6}));
+  assert(ts2.difference(ts1).equal({5,6}));
+
+  assert(ts1.cartesian_product(ts2).equal({{1,3},{1,4},{1,5},{1,6},{2,3},{2,4},{2,5},{2,6},{3,3},{3,4},{3,5},{3,6},{4,3},{4,4},{4,5},{4,6}}));
   
-  assert(s1.cartesian_product(s2).equal({{1,3},{1,4},{1,5},{1,6},{2,3},{2,4},{2,5},{2,6},{3,3},{3,4},{3,5},{3,6},{4,3},{4,4},{4,5},{4,6}}));
+  assert(ts1.cartesian_product(as2).equal({{1,3},{1,4},{1,5},{1,6},{2,3},{2,4},{2,5},{2,6},{3,3},{3,4},{3,5},{3,6},{4,3},{4,4},{4,5},{4,6}}));
 
-  assert(s1.zip(s2).equal({{1,3},{2,4},{3,5},{4,6}}));
+  assert(ts1.zip(ts2).equal({{1,3},{2,4},{3,5},{4,6}}));
   
   cout << "Everything ok!\n";
 
