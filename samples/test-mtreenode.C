94db3dc28e018e742cb1eab410fcb6b99f66a654
Alejandro <aledrums@gmail.com>
2018-03-14 09:37:17 -0400

    New methods to MTreeNode for asking whether a node has either siblings, parent or children. Moreover, you may add to a node p as a child or sibling whenever that p has neither parent or sibling

--- samples/test-mtreenode.C
+++ samples/test-mtreenode.C
@@ -18,12 +18,28 @@ int main()
   assert(root->get_right_sibling() == nullptr);
   assert(root->get_first_child() == nullptr);
   assert(root->get_last_child() == nullptr);
+  assert(not root->has_parent());
+  assert(not root->has_siblings());
+  assert(not root->has_children());
 
   Node * first_child = new Node('B');
+  assert(not first_child->has_parent());
+  assert(not first_child->has_siblings());
+  assert(not first_child->has_children());
   Node * second_child = new Node('C');
+  assert(not second_child->has_parent());
+  assert(not second_child->has_siblings());
+  assert(not second_child->has_children());
   Node * third_child = new Node('D');
+  assert(not third_child->has_parent());
+  assert(not third_child->has_siblings());
+  assert(not third_child->has_children());
 
   root->append_child(second_child);
+  assert(root->has_children());
+  assert(second_child->has_parent());
+  assert(not second_child->has_siblings());
+  assert(not second_child->has_children());
   assert(root->get_first_child() == second_child);
   assert(root->get_last_child() == second_child);
   assert(root->get_first_child()->get_left_sibling() == nullptr);
@@ -32,6 +48,11 @@ int main()
   assert(root->get_last_child()->get_right_sibling() == nullptr);
 
   root->insert_child(first_child);
+  assert(root->has_children());
+  assert(first_child->has_parent());
+  assert(first_child->has_siblings());
+  assert(second_child->has_siblings());
+  assert(not first_child->has_children());
   assert(root->get_first_child() == first_child);
   assert(root->get_last_child() == second_child);
   assert(root->get_first_child()->get_left_sibling() == nullptr);
@@ -40,6 +61,12 @@ int main()
   assert(root->get_last_child()->get_right_sibling() == nullptr);
 
   root->append_child(third_child);
+  assert(root->has_children());
+  assert(third_child->has_parent());
+  assert(first_child->has_siblings());
+  assert(second_child->has_siblings());
+  assert(third_child->has_siblings());
+  assert(not third_child->has_children());
   assert(root->get_first_child() == first_child);
   assert(root->get_last_child() == third_child);
   assert(root->get_first_child()->get_left_sibling() == nullptr);
@@ -53,6 +80,11 @@ int main()
 
   Node * grand_child = new Node('E');
   root->get_first_child()->get_right_sibling()->append_child(grand_child);
+  assert(root->has_children());
+  assert(grand_child->has_parent());
+  assert(not grand_child->has_siblings());
+  assert(second_child->has_children());
+  assert(not grand_child->has_children());
   assert(root->get_first_child()->get_right_sibling()->get_first_child()
 	 == grand_child);
   assert(root->get_first_child()->get_right_sibling()->get_last_child()
